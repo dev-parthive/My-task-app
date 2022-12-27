4d5743de22e542db421f1ee76d5dcafdb9142523
dev-parthive <dev-parthive>
2022-12-27 14:33:33 +0600

    NAVBAR done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Routes/Route';
+import 'flowbite'
 
 function App() {
   return (
